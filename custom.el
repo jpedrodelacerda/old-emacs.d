0a1ed0b66b5eb1c2fd17601c271b066c41f4233b
João Pedro de Lacerda <jpedrodelacerda@gmail.com>
2019-03-10 22:57:27 -0300

    Yay, new repo, new configuration.org, old setup.

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+	("8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
+ '(package-selected-packages
+   (quote
+	(spaceline-all-the-icons doom-modeline company-anaconda anaconda-mode anaconda-company company-jedi py-autopep8 python-mode racer flycheck-rust cargo rust-mode toml-mode smartparens web-mode elpy go-errcheck evil-nerd-commenter doom-themes tabbar yasnippet which-key use-package treemacs-evil solarized-theme smex org-bullets nlinum-relative neotree minions helm-projectile helm-company gruvbox-theme flycheck evil-surround evil-org evil-magit dumb-jump diff-hl counsel company-go auto-compile all-the-icons airline-themes ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
