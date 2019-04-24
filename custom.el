6934b3031e0076625838bd188b9436d819288498
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2019-04-24 14:02:52 -0300

    Ample theme.

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
 	("8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
  '(package-selected-packages
    (quote
-	(spaceline-all-the-icons doom-modeline company-anaconda anaconda-mode anaconda-company company-jedi py-autopep8 python-mode racer flycheck-rust cargo rust-mode toml-mode smartparens web-mode elpy go-errcheck evil-nerd-commenter doom-themes tabbar yasnippet which-key use-package treemacs-evil solarized-theme smex org-bullets nlinum-relative neotree minions helm-projectile helm-company gruvbox-theme flycheck evil-surround evil-org evil-magit dumb-jump diff-hl counsel company-go auto-compile all-the-icons airline-themes ag))))
+	(ample-theme bug-hunter yaml-mode ansible org-reveal ox-reveal company-ycmd ycmd company-box company-flx go-eldoc go-mode-eldoc go-mode treemacs projectile magit company helm spaceline-all-the-icons doom-modeline company-anaconda anaconda-mode anaconda-company company-jedi py-autopep8 python-mode racer flycheck-rust cargo rust-mode toml-mode smartparens web-mode elpy go-errcheck evil-nerd-commenter doom-themes tabbar yasnippet which-key use-package treemacs-evil solarized-theme smex org-bullets nlinum-relative neotree minions helm-projectile helm-company gruvbox-theme flycheck evil-surround evil-org evil-magit dumb-jump diff-hl counsel company-go auto-compile all-the-icons airline-themes ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 ;;(add-to-list 'package-archives
 ;;			 '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
