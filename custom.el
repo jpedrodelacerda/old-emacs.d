3b735a5c4a46c22f58b1690cda626313914a76be
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2019-05-01 09:54:01 -0300

    Updating some paths to support multiple environments: ycmd and gopath

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-	("8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
+	("2b8dff32b9018d88e24044eb60d8f3829bd6bbeab754e70799b78593af1c3aba" "b181ea0cc32303da7f9227361bb051bbb6c3105bb4f386ca22a06db319b08882" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" default)))
  '(package-selected-packages
    (quote
 	(ample-theme bug-hunter yaml-mode ansible org-reveal ox-reveal company-ycmd ycmd company-box company-flx go-eldoc go-mode-eldoc go-mode treemacs projectile magit company helm spaceline-all-the-icons doom-modeline company-anaconda anaconda-mode anaconda-company company-jedi py-autopep8 python-mode racer flycheck-rust cargo rust-mode toml-mode smartparens web-mode elpy go-errcheck evil-nerd-commenter doom-themes tabbar yasnippet which-key use-package treemacs-evil solarized-theme smex org-bullets nlinum-relative neotree minions helm-projectile helm-company gruvbox-theme flycheck evil-surround evil-org evil-magit dumb-jump diff-hl counsel company-go auto-compile all-the-icons airline-themes ag))))
