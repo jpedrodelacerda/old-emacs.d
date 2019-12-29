fa0ff031542981f43f5cc33ad330849dd43f158b
Joao Pedro de Lacerda <jpedrodelacerda@gmail.com>
2019-12-29 18:42:40 -0300

    Fixing powerline/airline missing icons

--- lisp/jpdl/ux.el
+++ lisp/jpdl/ux.el
@@ -49,7 +49,16 @@
 
 (use-package airline-themes
   :config
-  (load-theme 'airline-gruvbox-dark t))
+  (load-theme 'airline-gruvbox-dark t)
+  (setq powerline-utf-8-separator-left        #xe0b0
+		powerline-utf-8-separator-right       #xe0b2
+		airline-utf-glyph-separator-left      #xe0b0
+		airline-utf-glyph-separator-right     #xe0b2
+		airline-utf-glyph-subseparator-left   #xe0b1
+		airline-utf-glyph-subseparator-right  #xe0b3
+		airline-utf-glyph-branch              #xe0a0
+		airline-utf-glyph-readonly            #xe0a2
+		airline-utf-glyph-linenumber          #xe0a1))
 
 ;; Font
 (setq default-font "Fira Code")
